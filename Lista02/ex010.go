2ce98253311657db6da353ebca9bb09d711620ef
KezerSouza <166184042+KezerSouza@users.noreply.github.com>
2024-04-23 21:57:24 -0300

    Add files via upload

---
+++ Lista02/ex010.go
@@ -0,0 +1,16 @@
+//Centenas, dezenas, unidades
+package main
+import "fmt"
+
+func main() {
+    var horas, por_hora, salario float64
+    var matricula int
+    fmt.Scan(&matricula, &horas, &por_hora)
+ 
+    if(matricula != 0) {
+        salario = horas*por_hora
+        fmt.Printf("%v %0.2f\n", matricula, salario)
+        main()
+    }    
+    
+}
